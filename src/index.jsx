5aa2c33ca64d61273934dacf8005ac33126a2010
saufi <saufi@pdambandarmasih.com>
2024-01-23 19:07:27 +0800

    tambah style

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import ContactApp from "./ContactApp";
-
+// styling
+import "./style.css";
 const element = <h1> Hello,world mas bro !</h1>;
 const root = createRoot(document.getElementById("root"));
 root.render(<ContactApp />);
