3e385dfd0ae2a6012e298515988e1df09e808737
saufi <saufi@pdambandarmasih.com>
2024-01-24 19:48:28 +0800

    Revert "ubah ke functional component react hooks"
    This reverts commit 89c08c17eea6681cc6f9a6fa1736cae9aa08dc72.

--- src/ContactApp.jsx
+++ src/ContactApp.jsx
@@ -1,4 +1,4 @@
-//import React from "react";
+import React from "react";
 import ContactList from "./components/ContactList";
 import { getData } from "./data.js";
 
@@ -15,46 +15,27 @@ import { getData } from "./data.js";
 //   );
 // }
 
-// class ContactApp extends React.Component{
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       contacts: getData(),
-//     };
-//   }
-//   onDelete = (id) =>{
-//     const newContacts = this.state.contacts.filter((contact) => contact.id !== id);
-//     this.setState({
-//       contacts: newContacts,
-//     });
-//   }
-//   render(){
-//     return (
-//       <div className="contact-app">
-//         <h1>Daftar Kontak</h1>
-//         <ContactList contacts={this.state.contacts} onDelete={this.onDelete} />
-//       </div>
-//     );
-//   }
-// }
-import React, { useState } from 'react';
-
-const ContactApp = () => {
-  const [contacts, setContacts] = useState(getData());
-
-  const onDelete = (id) => {
-    const newContacts = contacts.filter((contact) => contact.id !== id);
-    setContacts(newContacts);
-  };
-
-  return (
-    <div className="contact-app">
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={contacts} onDelete={onDelete} />
-    </div>
-  );
-};
-
-
+class ContactApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      contacts: getData(),
+    };
+  }
+  onDelete = (id) =>{
+    const newContacts = this.state.contacts.filter((contact) => contact.id !== id);
+    this.setState({
+      contacts: newContacts,
+    });
+  }
+  render(){
+    return (
+      <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <ContactList contacts={this.state.contacts} onDelete={this.onDelete} />
+      </div>
+    );
+  }
+}
 
 export default ContactApp;
