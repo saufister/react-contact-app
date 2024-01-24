b32ee5bb21a471ac0f9239729769bb0e0cef516e
PDAMBJM\saufi <saufi@pdambandarmasih.com>
2024-01-24 15:50:05 +0800

    worked help by copilot

--- src/ContactApp.jsx
+++ src/ContactApp.jsx
@@ -2,14 +2,40 @@ import React from "react";
 import ContactList from "./components/ContactList";
 import { getData } from "./data.js";
 
-function ContactApp() {
-  const contacts = getData();
-  return (
-    <div className="contact-app">
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={contacts} />
-    </div>
-  );
+// function onDelete(){
+//   console.log("Delete");
+// }
+// function ContactApp() {
+//   const contacts = getData();
+//   return (
+//     <div className="contact-app">
+//       <h1>Daftar Kontak</h1>
+//       <ContactList contacts={contacts} onDelete={onDelete} />
+//     </div>
+//   );
+// }
+
+class ContactApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      contacts: getData(),
+    };
+  }
+  onDelete = (id) =>{
+    const newContacts = this.state.contacts.filter((contact) => contact.id !== id);
+    this.setState({
+      contacts: newContacts,
+    });
+  }
+  render(){
+    return (
+      <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <ContactList contacts={this.state.contacts} onDelete={this.onDelete} />
+      </div>
+    );
+  }
 }
 
 export default ContactApp;
