b1b06bff30dc159c33467d48582f140c110de9a4
saufi <saufi@pdambandarmasih.com>
2024-01-24 19:48:11 +0800

    Revert "coba install react-boostrap dan gunakan button"
    This reverts commit 431ca40bb649f8e7f1c487ba5d183fca96b452a3.

--- src/ContactApp.jsx
+++ src/ContactApp.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+//import React from "react";
 import ContactList from "./components/ContactList";
 import { getData } from "./data.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Button, Col } from "react-bootstrap";
+
 // function onDelete(){
 //   console.log("Delete");
 // }
@@ -16,52 +15,46 @@ import { Container, Button, Col } from "react-bootstrap";
 //   );
 // }
 
-class ContactApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: getData(),
-    };
-  }
-  onDelete = (id) => {
-    const newContacts = this.state.contacts.filter(
-      (contact) => contact.id !== id
-    );
-    this.setState({
-      contacts: newContacts,
-    });
+// class ContactApp extends React.Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       contacts: getData(),
+//     };
+//   }
+//   onDelete = (id) =>{
+//     const newContacts = this.state.contacts.filter((contact) => contact.id !== id);
+//     this.setState({
+//       contacts: newContacts,
+//     });
+//   }
+//   render(){
+//     return (
+//       <div className="contact-app">
+//         <h1>Daftar Kontak</h1>
+//         <ContactList contacts={this.state.contacts} onDelete={this.onDelete} />
+//       </div>
+//     );
+//   }
+// }
+import React, { useState } from 'react';
+
+const ContactApp = () => {
+  const [contacts, setContacts] = useState(getData());
+
+  const onDelete = (id) => {
+    const newContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(newContacts);
   };
-  onTambah = () => {
-    alert("tombol ditambah mas bro");
-  };
-  render() {
-    return (
-      <div className="contact-app">
-        <h1>Daftar Kontak</h1>
-        <Button onClick={this.onTambah} as="a" variant="primary">
-          Tambah
-        </Button>
-        <ContactList contacts={this.state.contacts} onDelete={this.onDelete} />
-      </div>
-    );
-  }
-}
-// import React, { useState } from 'react';
 
-// const ContactApp = () => {
-//   const [contacts, setContacts] = useState(getData());
+  return (
+    <div className="contact-app">
+      <h1>Daftar Kontak</h1>
+      <ContactList contacts={contacts} onDelete={onDelete} />
+    </div>
+  );
+};
 
-//   const onDelete = (id) => {
-//     const newContacts = contacts.filter((contact) => contact.id !== id);
-//     setContacts(newContacts);
-//   };
 
-//   return (
-//     <div className="contact-app">
-//       <h1>Daftar Kontak</h1>
-//       <ContactList contacts={contacts} onDelete={onDelete} />
-//     </div>
-//   );
-// };
 
 export default ContactApp;
--- src/data.js
+++ src/data.js
@@ -17,19 +17,7 @@ function getData(){
             name: 'Rahmat Fajri',
             tag: 'rfajri27',
             imageUrl: '/images/rfajri27.jpeg',
-          },     
-          {
-            id: 4,
-            name: 'Muhammad saufi Fajri',
-            tag: 'rfajri27',
-            imageUrl: '/images/rfajri27.jpeg',
-          },      
-          {            
-            id : 5, 
-            name : 'Dimas Dimas',
-            tag : 'dimasmds',
-            imageUrl : '/images/dimasmds.jpeg',
-        },   
+          },        
     ];
 }
 export {getData};
