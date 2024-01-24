a7e90bd36167a6b8331c5ffd402b47b61c4372d2
PDAMBJM\saufi <saufi@pdambandarmasih.com>
2024-01-24 15:49:15 +0800

    add button delete component

---
+++ src/components/DeleteButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function DeleteButton({id,onDelete}){
+    return (
+        <button className = 'contact-item__delete' onClick={()=>onDelete(id)}>x</button>
+    )
+}
+
+export default DeleteButton;
